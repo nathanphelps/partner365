fd557d03ec681c9873b22b8fba196e3dd748446d
Nathan Phelps <nathanphelps@gmail.com>
2026-03-04 22:14:07 -0600

    Update AppLogo.vue

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -3,12 +3,8 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
 </script>
 
 <template>
-    <div
-        class="flex aspect-square size-8 items-center justify-center rounded-md bg-white/10"
-    >
-        <AppLogoIcon class="size-5 text-indigo-300" />
-    </div>
-    <div class="ml-1 grid flex-1 text-left text-sm">
+    <AppLogoIcon class="!size-7 text-indigo-300 shrink-0" />
+    <div class="ml-1 grid flex-1 text-left text-base">
         <span class="mb-0.5 truncate leading-tight font-semibold"
             >Partner365</span
         >
